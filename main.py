9792934bcfc9691c6835cf51582bc955dac825f5
detodopcs <detodopcs@hotmail.com>
2024-11-17 23:13:14 -0600

    Mensaje2 que describe los cambios

---
+++ main.py
@@ -0,0 +1,16 @@
+
+from sumar import sumar_2_numeros
+from dividir import dividir_2_numeros
+from multiplicar import multiplicar_2_numeros
+from restar import restar_2_numeros
+from suma_avanzada import suma_avanzada_numeros
+
+#funciones
+sumar =  sumar_2_numeros(10,62) 
+dividir = dividir_2_numeros(60,90)
+multiplicar = multiplicar_2_numeros(50,5)
+restar = restar_2_numeros(100,75)
+suma_avanzada = suma_avanzada_numeros(100,550,66,99)
+
+print(suma_avanzada)
+
